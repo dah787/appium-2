f87b949c9ee19da65293f41bb07f60ad1cf15d7b
dah787 <dah787@yandex.ru>
2023-08-06 12:55:16 +0500

    Test changes #6

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -35,8 +35,8 @@ config.capabilities = [{
   "appium:platformName": "Android",
 
   "appium:platformVersion": "10.0",
-  "appium:deviceName": "Nexus S v.10",
-  // "appium:deviceName": "Pixel 4 v.10",
+  // "appium:deviceName": "Nexus S v.10",
+  "appium:deviceName": "Pixel 4 v.10",
   // "appium:platformVersion": "11.0",
   // "appium:deviceName": "Pixel 4 v.11",
   
