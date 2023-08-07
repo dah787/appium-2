0fb8a4fae2a54d98f6bb2b90faf7d4e01d96574b
dah787 <dah787@yandex.ru>
2023-08-07 10:46:56 +0500

    Test changes #14

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -52,7 +52,7 @@ config.capabilities = [{
   // "appium:platformVersion": "13.0",
   // "appium:deviceName": "Pixel XL v.13",
   "appium:platformVersion": "13.0",
-  "appium:deviceName": "Pixel 7 v.13",
+  "appium:deviceName": "Pixel 7 v.13", //fgh
 
   "appium:automationName": "UiAutomator2",
   "appium:app": path.join(process.cwd(), "app/android/Apexbank-1.0.17.1-debug.apk"),
