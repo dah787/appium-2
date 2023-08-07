9ad5ed3fb39ce32603bdd7dbdb424f8691b99640
dah787 <dah787@yandex.ru>
2023-08-07 14:20:05 +0500

    Test changes #21

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -52,7 +52,7 @@ config.capabilities = [{
   // "appium:platformVersion": "13.0",
   // "appium:deviceName": "Pixel XL v.13",
   "appium:platformVersion": "13.0",
-  "appium:deviceName": "Pixel 7 v.13",  // 1080*2400, 420 dpi, 6.31"
+  "appium:deviceName": "Pixel 7 v.13",    // 1080*2400, 420 dpi, 6.31"
   // "appium:platformVersion": "14.0",
 
   "appium:automationName": "UiAutomator2",
