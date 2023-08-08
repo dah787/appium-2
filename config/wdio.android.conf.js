d34ef1315ab502f1e3e0484cfb7c0a37fe6c95a4
dah787 <dah787@yandex.ru>
2023-08-08 12:17:45 +0500

    Test changes #39

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -37,7 +37,7 @@ config.capabilities = [{
 
   "appium:platformName": "Android",
 
-  // "appium:platformVersion": "10.0",
+  "appium:platformVersion": "10.0",
   // "appium:deviceName": "Nexus S v.10", // 480*800, 240 dpi, 4.0"
   // "appium:deviceName": "Pixel 4 v.10",
   // "appium:platformVersion": "11.0",
@@ -54,7 +54,7 @@ config.capabilities = [{
   // "appium:deviceName": "Pixel 6 v.13",
   // "appium:platformVersion": "13.0",
   // "appium:deviceName": "Pixel XL v.13",
-  "appium:platformVersion": "13.0",
+  // "appium:platformVersion": "13.0",
   "appium:deviceName": "Pixel 7 v.13", // 1080*2400, 420 dpi, 6.31"
   // "appium:platformVersion": "14.0",
 
