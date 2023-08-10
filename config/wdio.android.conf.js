6edf66952e09c5c43728c48b807181767bb1b9a1
dah787 <dah787@yandex.ru>
2023-08-10 14:48:01 +0500

    Test change #1

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -38,14 +38,16 @@ config.capabilities = [{
   "appium:platformName": "Android",
 
   "appium:platformVersion": "10.0",
-  // "appium:deviceName": "Nexus S v.10", // 480*800, 240 dpi, 4.0"
+  // "appium:deviceName": "Nexus S v.10",   //   480*800, 240 dpi, 4.0", x86
+  "appium:deviceName": "Pixel 2 v.10",   // 1080*1920, 420 dpi, 5.0", x86
   // "appium:deviceName": "Pixel 4 v.10",
+  
   // "appium:platformVersion": "11.0",
   // "appium:deviceName": "Pixel 4 v.11",
   
   // "appium:platformVersion": "12.0",
   // "appium:platformVersion": "12L",
-  // "appium:deviceName": "Pixel 4 v.12L", // 1080*2280, 440 dpi, 6.3"
+  // "appium:deviceName": "Pixel 4 v.12L",  // 1080*2280, 440 dpi, 6.3", x86_64
   // "appium:platformVersion": "12L",
   // "appium:deviceName": "Pixel 4a v.12L",
 
@@ -56,8 +58,7 @@ config.capabilities = [{
   // "appium:platformVersion": "13.0",
   // "appium:deviceName": "Pixel XL v.13",
   // "appium:platformVersion": "13.0",
-  "appium:deviceName": "Pixel 7 v.13", // 1080*2400, 420 dpi, 6.31"
-  // "appium:platformVersion": "14.0",
+  // "appium:deviceName": "Pixel 7 v.13",   // 1080*2400, 420 dpi, 6.31", x86_64
 
   "appium:automationName": "UiAutomator2",
   "appium:app": path.join(process.cwd(), "app/android/Apexbank-1.0.17.1-debug.apk"),
