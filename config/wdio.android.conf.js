bee8cdbdf48f0e652dfdb969d4b5108acdb17e2f
dah787 <dah787@yandex.ru>
2023-08-16 13:38:09 +0500

    Test changes #53

--- config/wdio.android.conf.js
+++ config/wdio.android.conf.js
@@ -47,7 +47,7 @@ config.capabilities = [{
 
   "appium:platformVersion": "13.0",
   // "appium:deviceName": "Pixel 6a v.13",   // 1080*2400, 420 dpi, 6.13", x86_64
-  "appium:deviceName": "Pixel 7 v.13",   // 1080*2400, 420 dpi, 6.31", x86_64 < "Pixel_7_API_33"
+  "appium:deviceName": "Pixel 7 v.13",   // 1080*2400, 420 dpi, 6.31", x86_64 << "Pixel_7_API_33"
 
   "appium:automationName": "UiAutomator2",
   // "appium:app": path.join(process.cwd(), "app/android/Apexbank-1.0.17.1-debug.apk"),
