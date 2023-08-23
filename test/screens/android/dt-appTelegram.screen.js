dab2b7a10c9a27866acfc9613d387ec920b2bece
dah787 <dah787@yandex.ru>
2023-08-23 13:59:54 +0500

    Change #63

--- test/screens/android/dt-appTelegram.screen.js
+++ test/screens/android/dt-appTelegram.screen.js
@@ -4,7 +4,7 @@ class AppTelegramScreen { // related to device tested
 get supportContactTelegramName() {
   return $('//android.widget.TextView[@text="Apexbank"]');} // "Jaksibay Khakimov"
 get supportContactTelegramJoinButton() {
-  return $('//android.view.View[@text="ПРИСОЕДИНИТЬСЯ"]');}
+  return $('//android.view.View[@content-desc="ПРИСОЕДИНИТЬСЯ"]');}
 get supportContactTelegramMessageInput() {
   return $('//android.widget.EditText[@text="Message"]');}
 
